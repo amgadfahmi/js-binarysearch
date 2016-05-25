d9a08df806ae8a03703fa171178026a0d1db4712
Amgad Fahmi <amgadnady@gmail.com>
2016-05-25 17:41:38 +0400

    Init 2
    Signed-off-by: Amgad Fahmi <amgadnady@gmail.com>

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![Build Status](https://secure.travis-ci.org/amgadfahmi/js-binarysearch.png?branch=master)](http://travis-ci.org/amgadfahmi/js-binarysearch)
 
-Binary search algorithm implementation in Javascript without any dependency 
+Binary search algorithm implementation in Javascript without any dependency.
 
 ## Installation
 
@@ -12,7 +12,13 @@ $ npm install --save-dev js-binarysearch
 
 ## Usage
 
+```
+$ bower install --s js-binarysearch
+```
+
 ## Documentation
+
+####coming soon....
 
 ## API
 
---
+++ dist/js-binarysearch.js
@@ -0,0 +1,29 @@
+/*! js-binarysearch - v0.0.0
+ *  Release on: 2016-05-25
+ *  Copyright (c) 2016 Amgad Fahmi
+ *  Licensed MIT */
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module unless amdModuleId is set
+    define([], function () {
+      return (factory());
+    });
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  } else {
+    factory();
+  }
+}(this, function () {
+
+'use strict';
+
+// code goes here
+var jsbinarysearch = function () {
+
+};
+
+
+}));
